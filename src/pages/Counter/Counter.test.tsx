934fcf4887b160cadc92a5239c9fe7792a8d5d2d
Vinod Kumar Nandimandalam <vinodkumarnandimandalam@Vinods-MacBook-Pro.local>
2025-11-13 14:30:30 +0530

    Adds counter component

---
+++ src/pages/Counter/Counter.test.tsx
@@ -0,0 +1,30 @@
+// Check if initial count is displayed properly or not
+// Check when increment button pressed count should increase
+// Check when decrement button pressed count should decrease
+import Counter from './Counter';
+import { fireEvent, render } from '@testing-library/react-native';
+
+export const getDisplayText = (count: number) => `Count ${count}`;
+export const INCREMENT_BUTTON = 'INCREMENT';
+export const DECREMENT_BUTTON = 'DECREMENT';
+
+describe('Counter component', () => {
+  it('Should render the correct initial count', () => {
+    const { getByText } = render(<Counter />);
+    expect(getByText(getDisplayText(0))).toBeDefined();
+  });
+
+  it('Should increment the count on pressing increment button', () => {
+    const { getByText } = render(<Counter />);
+    const incrementBtn = getByText(INCREMENT_BUTTON);
+    fireEvent.press(incrementBtn);
+    expect(getByText(getDisplayText(1))).toBeDefined();
+  });
+
+  it('Should decrement the count on pressing decrement button', () => {
+    const { getByText } = render(<Counter />);
+    const decrementBtn = getByText(DECREMENT_BUTTON);
+    fireEvent.press(decrementBtn);
+    expect(getByText(getDisplayText(-1))).toBeDefined();
+  });
+});
---
+++ src/pages/Counter/Counter.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import {
+  DECREMENT_BUTTON,
+  getDisplayText,
+  INCREMENT_BUTTON,
+} from './Counter.test';
+import Button from '../../components/Button/Button';
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(prevCount => prevCount + 1);
+  };
+
+  const handleDecrement = () => {
+    setCounter(prevCount => prevCount - 1);
+  };
+
+  return (
+    <View>
+      <Text>{getDisplayText(counter)}</Text>
+      <Button title={INCREMENT_BUTTON} onPress={handleIncrement} />
+      <Button title={DECREMENT_BUTTON} onPress={handleDecrement} />
+    </View>
+  );
+};
+
+export default Counter;
